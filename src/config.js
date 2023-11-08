95e7d0b264213be270d3c37cddc1504b9a189193
ADG <2007andrepaiva@gmail.com>
2023-11-08 18:04:16 -0300

    Basic

---
+++ src/config.js
@@ -0,0 +1,34 @@
+const emojis = require('./utils/emojis.json')
+
+module.exports = {
+  client: {
+    token: process.env.CLIENT_ID,
+    id: process.env.CLIENT_ID,
+  },
+  handler: {
+    prefix: "s!",
+    deploy: true,
+    commands: {
+      prefix: true,
+      slash: true,
+      user: true,
+      message: true,
+    },
+    mongodb: {
+      uri: process.env.MONGODB_URI,
+      toggle: false,
+    },
+  },
+  users: {
+    developers: ["717766639260532826"],
+  },
+  messageSettings: {
+    nsfwMessage: `${emojis.error} **|** Este canal não é um canal NSFW.`,
+    developerMessage: `${emojis.error} **|** Você não possui autorização para utilizar este comando.`,
+    cooldownMessage: `${emojis.error} **|** Calma ai, amigo! Aguarde para usar este comando.`,
+    notHasPermissionMessage:
+    `${emojis.error} **|** Você não tem permissão para usar esse comando.`,
+    missingDevIDsMessage:
+    `${emojis.error} **|** Esse é um comando exclusivo para desenvolvedores..`,
+  },
+};
